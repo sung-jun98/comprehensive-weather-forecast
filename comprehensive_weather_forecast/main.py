5e6fc94696a60e3bcbbc68a4c5850441d7ee31ef
김성준 <88644396+sung-jun98@users.noreply.github.com>
2022-02-15 21:42:14 +0900

    Add files via upload
    글꼴 깨짐 문제 수정/ 파이썬 schedule 라이브러리를 사용하여 오후 11시에 19지역 기온 크롤링하도록 설정/

--- comprehensive_weather_forecast/main.py
+++ comprehensive_weather_forecast/main.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, render_template, make_response, session
 from class_list import crawling_and_graph
 from flask_cors import CORS
+import schedule
 import os
+dict_of_landname = {'서울': 'Seoul', '부산': 'Busan', '대구': 'Daegu', '대전' : 'Daejeon', '광주' : 'Gwangju', '수원': 'Suwon', '울산': 'Ulsan', '청주': 'Cheongju-si', '제주' : 'Jeju-si', '춘천':'Chuncheon', '속초': "Sokcho", '김포국제공항' : 'Gimpo International Airport', '포항' : 'Pohand', '김해':'Gimhae' , '진주': 'Jinju', '목포' : 'Moppo', '군산' : 'Kunsan' , '원주' : 'Wonju', '강릉' : 'Kang-neung' }
 
-#weather = crawling_and_graph.Weather_test('서울')
-#weather.bbc_weather()
-#weather.accu_weather()
-#weather.kma_weather()
-#weather.twc_weather()
-#△5줄 : 파이썬의 schedule 라이브러리를 사용하여 일정시간에 실행되도록 수정할 것(하루에 한번씩)
+def crawling_everyday():
+    for i in dict_of_landname.keys():
+        weather = crawling_and_graph.Weather_test(i)
+        weather.bbc_weather()
+        weather.accu_weather()
+        weather.kma_weather()
+        weather.twc_weather()
+        print(i + '지역 크롤링 완료')
+        
+schedule.every().day.at("23:00").do(crawling_everyday)
+#△5줄 : 파이썬의 schedule 라이브러리를 사용하여 매일 오후 11시에 크롤링 클래스가 실행하도록 설정.
 
-#app = Flask(__name__, static_url_path='/static')
+
 app = Flask(__name__)
 CORS(app)
 @app.route('/') #맨 처음 접속했을 때의 시작페이지 리턴
